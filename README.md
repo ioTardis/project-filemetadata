97c8451fd1cac0553c3a3dee55c118e73df0b9dc
Izabella Golovina <62325997+ioTardis@users.noreply.github.com>
2022-09-03 12:07:21 +0000

    edit README file change style.css and index.html for webpage appearance

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # project-filemetadata
+
+This is a Node.js (with Express.js) small app that takes file from user using multer and sends back JSON object with name, type and size of the file. File is stored in memory and once request is done memory will be freed.
+
+## Example output
+
+```json
+{"name":"css-grid-poster.png","type":"image/png","size":1616679}
+```
+
+## [Try it](https://project-filemetadata.iotardis.repl.co)
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let express = require('express');
-let cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ storage: multer.memoryStorage()});
 require('dotenv').config()
 
-let app = express();
+const app = express();
 
 app.use(cors());
 // serving static file
@@ -18,4 +20,9 @@ const port = process.env.PORT || 3000;
 // listen the port
 app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
+});
+
+// file analyser
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  res.json( { name: req.file.originalname, type: req.file.mimetype, size: req.file.size} );
 });
